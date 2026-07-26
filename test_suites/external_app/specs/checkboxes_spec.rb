54f0e2a813e9d8861b9bd5f919e0e612b142e458
Nielson Rolim <nielson.rolim@gmail.com>
2026-07-25 22:09:17 -0300

    Create checkboxes_spec.rb

---
+++ test_suites/external_app/specs/checkboxes_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+RSpec.describe "Checkboxes" do
+  it "starts with the first checkbox unchecked and the second checked" do
+    visit "/checkboxes"
+
+    checkboxes = all("#checkboxes input[type=checkbox]")
+    expect(checkboxes[0]).not_to be_checked
+    expect(checkboxes[1]).to be_checked
+  end
+
+  it "toggles a checkbox's checked state when clicked" do
+    visit "/checkboxes"
+
+    first_checkbox = all("#checkboxes input[type=checkbox]").first
+    first_checkbox.click
+    expect(first_checkbox).to be_checked
+
+    first_checkbox.click
+    expect(first_checkbox).not_to be_checked
+  end
+end
